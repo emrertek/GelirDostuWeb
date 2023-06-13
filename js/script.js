cf6c0b763c2c0fd4c09364a1a42ccebc7173dc16
ugurcanertek <ugurcan.ertek@useinsider.com>
2023-06-13 03:55:26 +0300

    Search Result Container Added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,217 @@
+var allProducts = [{
+        id: 0,
+        img: 'images/BB_KREM_.jpg',
+        category: 'Sağlık ve Bakım',
+        name: 'BB krem',
+        price: 200.0,
+        href: 'https://www.cimri.com/fondotenler/en-ucuz-pure-beauty-spf50-pa-ivory-30-ml-bb-cream-fiyatlari,2088770791',
+        quantity: 1
+    },
+    {
+        id: 1,
+        img: 'images/box2image.jpg',
+        category: 'Teknoloji',
+        name: 'Laptop',
+        price: 9999,
+        href: 'https://www.cimri.com/dizustu-bilgisayar/en-ucuz-huawei-matebook-d15-i3-1115g4-8-gb-ram-256-gb-ssd-w11h-fhd-dizustu-bilgisayar-fiyatlari,2019887259',
+        quantity: 1
+    },
+    {
+        id: 2,
+        img: 'images/box4.jpg',
+        category: 'Teknoloji',
+        name: 'TABLET',
+        price: 2199,
+        href: 'https://www.cimri.com/tablet-pc/en-ucuz-samsung-galaxy-tab-a7-lite-t220-32gb-8-7-tablet-pc-gri-fiyatlari,884160691',
+        quantity: 1
+    },
+    {
+        id: 3,
+        img: 'images/box5.jpg',
+        category: 'Moda',
+        name: 'NEW BALANCE',
+        price: 2792,
+        href: 'https://www.cimri.com/spor-ayakkabi/en-ucuz-new-balance-mr530sg-530-unisex-spor-ayakkabi-fiyatlari,1663295771',
+        quantity: 1
+    },
+    {
+        id: 4,
+        img: 'images/box6.jpeg',
+        category: 'Ev, Yaşam ve Ofis',
+        name: 'MASA LAMBASI',
+        price: 206.5,
+        href: 'https://www.cimri.com/masa-lambasi/en-ucuz-cata-ct-8405-new-york-masa-lambasi-fiyatlari,743195232',
+        quantity: 1
+    },
+    {
+        id: 5,
+        img: 'images/casper-via-e30-beyaz-13_op_tekno.jpg',
+        category: 'Teknoloji',
+        name: 'CEP TELEFONU',
+        price: 3200.5,
+        href: 'https://www.akakce.com/cep-telefonu/en-ucuz-casper-via-e30-64-gb-fiyati,1364920356.html',
+        quantity: 1
+    },
+    {
+        id: 6,
+        img: 'images/ev_yasam3.jpg',
+        category: 'Ev, Yaşam ve Ofis',
+        name: 'OFİS MASA ',
+        price: 469.9,
+        href: 'https://www.hepsiburada.com/tomar-metal-calisma-bilgisayar-masasi-ofis-masasi-ceviz-50x120-cm-pm-HB0000135XVM',
+        quantity: 1
+    },
+    {
+        id: 7,
+        img: 'images/ev-yasam1.jpg',
+        category: 'Ev, Yaşam ve Ofis',
+        name: 'KİTAPLIK',
+        price: 124.99,
+        href: 'https://www.hepsiburada.com/bambeds-mobilya-dekor-cok-amacli-mini-rafli-masa-ve-tezgah-ustu-dolap-pm-HBC00002GWGS3',
+        quantity: 1
+    },
+    {
+        id: 8,
+        img: 'images/ev-yasam2.jpg',
+        category: 'Ev, Yaşam ve Ofis',
+        name: 'OFİS KOLTUK',
+        price: 575.0,
+        href: 'https://www.hepsiburada.com/aym-ofis-kumas-doseme-siyah-sekreter-ofis-ev-ogrenci-calisma-sandalyesi-pm-HBC000030TDEL',
+        quantity: 1
+    },
+    {
+        id: 9,
+        img: 'images/GD_bakim_yagi.jpg',
+        category: 'Sağlık ve Bakım',
+        name: 'SAÇ BAKIM YAĞI',
+        price: 44.9,
+        href: 'https://www.cimri.com/masa-lambasi/en-ucuz-cata-ct-8405-new-york-masa-lambasi-fiyatlari,743195232',
+        quantity: 1
+    },
+    {
+        id: 10,
+        img: 'images/GD_saglik_bakim_4.jpg',
+        category: 'Sağlık ve Bakım',
+        name: 'TER ÖNLEYİCİ SPREY',
+        price: 89.78,
+        href: 'https://www.trendyol.com/tereson/el-ayak-ve-koltuk-alti-terleme-onleyici-sprey-50-ml-p-125379351?boutiqueId=61&merchantId=110964',
+        quantity: 1
+    },
+    {
+        id: 11,
+        img: 'images/GITAR_.jpg',
+        category: 'Hobi',
+        name: 'GİTAR',
+        price: 427.50,
+        href: 'https://www.cimri.com/klasik-gitarlar/en-ucuz-jwin-cg-2101-naturel-soprano-ukulele-gitar-fiyatlari,514884162',
+        quantity: 1
+    },
+    {
+        id: 12,
+        img: 'images/HOBİ-1.jpg',
+        category: 'Hobi',
+        name: 'KAYKAY',
+        price: 211.99,
+        href: 'https://www.trendyol.com/triathlon/t132-rip-curl-desenli-kaykay-p-104259696',
+        quantity: 1
+    },
+    {
+        id: 13,
+        img: 'images/HOBİ-2.jpg',
+        category: 'Hobi',
+        name: 'BİSİKLET',
+        price: 1899,
+        href: 'https://www.trendyol.com/serraro/20-jant-bisiklet-denge-tekerli-p-336317296?boutiqueId=61&merchantId=682855',
+        quantity: 1
+    },
+    {
+        id: 14,
+        img: 'images/MODA-2.jpg',
+        category: 'Moda',
+        name: 'ERKEK TİŞÖRT',
+        price: 179.99,
+        href: 'https://www.hepsiburada.com/mavi-erkek-miav-baskili-beyaz-tisort-067153-620-p-HBCV000016AHOD',
+        quantity: 1
+    },
+    {
+        id: 15,
+        img: 'images/hazir_sistem.jpg',
+        category: 'Teknoloji',
+        name: 'HAZIR SİSTEM',
+        price: 11399,
+        href: 'https://www.gencergaming.com/pch-v3.1',
+        quantity: 1
+    }
+];
+
+var appendSearchResultContainer = function () {
+    if ($('.search-product-container').length > 0) {
+        return;
+    }
+
+    $('.search-form').append('<div class="search-product-container">');
+    $('.search-product-container').append('<ul class="search-product-list">');
+}
+
+var appendSearchResultProduct = function (product) {
+    var $searchContainer = $('.search-product-list');
+    var productHTML = '' +
+        '<li class="search-product">' +
+        '   <div class="search-product-image"><img src="{Image}"></div>' +
+        '   <div class="search-product-attributes">' +
+        '       <div class="search-product-category">{Category}</div>' +
+        '       <div class="search-product-name">{ProductName}</div>' +
+        '       <div class="search-product-price">' +
+        '           <div class="discounted-price">{DiscountedPrice}</div>' +
+        '           <div class="original-price">{OriginalPrice}</div>' +
+        '       </div>' +
+        '   </div>' +
+        '</li>';
+
+    if ($searchContainer.length === 0) {
+        return;
+    }
+
+    $searchContainer.append(
+        productHTML
+        .replace('{Image}', product.img)
+        .replace('{Category}', product.category)
+        .replace('{ProductName}', product.name)
+        .replace('{DiscountedPrice}', (product.price * 0.8).toFixed(2))
+        .replace('{OriginalPrice}', product.price)
+    );
+}
+
+var listSearchResult = function () {
+    var value = $(this).val().toLowerCase();
+    var $searchProducts = $('.search-product-list .search-product');
+
+    if ($searchProducts.length > 0) {
+        $searchProducts.remove();
+    }
+
+    allProducts.forEach(function (product) {
+        if (!product.name.toLowerCase().includes(value)) {
+            return;
+        }
+
+        appendSearchResultContainer();
+        appendSearchResultProduct(product);
+    });
+}
+
 var searchform = document.querySelector(".search-form");
 var navbar = document.querySelector(".navbar");
+var $searchInput = document.querySelector('.search-input');
 
 //! buttons 
 var searchbtn = document.querySelector("#search-btn");
 var rspbtn = document.querySelector("#rsp-btn");
 
-searchbtn.addEventListener("click",function(){
+searchbtn.addEventListener("click", function () {
     searchform.classList.toggle("active");
 
-    document.addEventListener("click",function (event) {
+    document.addEventListener("click", function (event) {
         if (!event.composedPath().includes(searchbtn) && !event.composedPath().includes(searchform)) {
             searchform.classList.remove("active");
         }
@@ -25,8 +228,20 @@ rspbtn.addEventListener("click", function () {
     })
 });
 
-var sil = function () { 
+$searchInput.addEventListener('keyup', listSearchResult);
+
+var loadJQuery = function () {
+    var script = document.createElement('script');
+
+    script.src = 'js/code.jquery.com_jquery-3.7.0.js';
+
+    document.getElementsByTagName('body')[0].appendChild(script);
+};
+
+var sil = function () {
     var metin = document.getElementById('intro');
 
-    metin.value = "" ; 
+    metin.value = "";
 };
+
+loadJQuery();
